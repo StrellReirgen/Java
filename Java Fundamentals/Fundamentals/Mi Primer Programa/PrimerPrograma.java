446b3aec19838d0e1c22eb972f671932ed5e3d5c
Marcelo Negrier <60198827+StrellReirgen@users.noreply.github.com>
2020-02-28 09:31:37 -0300

    Subiendo primer programa java

---
+++ Java Fundamentals/Fundamentals/Mi Primer Programa/PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Marcelo Negrier");
+        System.out.println("Tengo 26 años de edad.");
+        System.out.println("Y mi ciudad natal es Lonquimay, CL");
+    }
+}
