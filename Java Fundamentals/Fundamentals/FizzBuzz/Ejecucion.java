ea70b6adf28f7d984314fc13e0c7e6edb95745ae
Marcelo Negrier <60198827+StrellReirgen@users.noreply.github.com>
2020-03-02 09:17:54 -0300

    Subiendo FizzBuzz

---
+++ Java Fundamentals/Fundamentals/FizzBuzz/Ejecucion.java
@@ -0,0 +1,7 @@
+public class Ejecucion {
+    public static void main(String[] args) {
+        FizzBuzz iD = new FizzBuzz();
+        String resultado = iD.fizzBuzz(13452);
+        System.out.println(resultado);
+    }
+}
