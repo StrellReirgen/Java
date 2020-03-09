7e256096227e466797dd567a1f0b82afbbef0954
Marcelo Negrier <60198827+StrellReirgen@users.noreply.github.com>
2020-03-09 17:39:03 -0300

    Actualizado Clases

--- Java Fundamentals/OOP/src/objectmaster/HumanTest.java
+++ Java Fundamentals/OOP/src/objectmaster/HumanTest.java
@@ -3,11 +3,11 @@ package objectmaster;
 public class HumanTest {
 
 	public static void main(String[] args) {
-		Human humano1 = new Human();
-		Human humano2 = new Human();
-		humano1.attack(humano2);
-		humano1.displayHealth();
-		humano2.displayHealth();
+		//Human humano1 = new Human();
+		//Human humano2 = new Human();
+		//humano1.attack(humano2);
+		//humano1.displayHealth();
+		//humano2.displayHealth();
 		Wizard merlin = new Wizard();
 		Ninja raiden = new Ninja();
 		Samurai kojiro = new Samurai();
@@ -16,6 +16,15 @@ public class HumanTest {
 		raiden.steal(gintoki);
 		raiden.runAway();
 		merlin.fireBall(raiden);
+		merlin.heal(gintoki);
+		kojiro.deathBlow(musashi);
+		kojiro.meditate();
+		System.out.println(gintoki.howMany());
+		merlin.displayHealth();
+		raiden.displayHealth();
+		kojiro.displayHealth();
+		musashi.displayHealth();
+		gintoki.displayHealth();
 		
 	}
 }
