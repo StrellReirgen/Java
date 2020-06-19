f68778b747e3b32cd7fe739c771dbdb76f128690
Marcelo Negrier <60198827+StrellReirgen@users.noreply.github.com>
2020-06-19 14:55:16 -0400

    update

--- Java Fundamentals/OOP_Advanced/src/cuentabanco/BancoTest.java
+++ Java Fundamentals/OOP_Advanced/src/cuentabanco/BancoTest.java
@@ -8,13 +8,24 @@ public class BancoTest {
 		BankAccount cuenta2 = new BankAccount();
 		BankAccount cuenta3 = new BankAccount();
 		cuenta.datos();
-		cuenta.deposito("corriente", 100);
+		cuenta3.deposito("corriente", 100);
 		cuenta.deposito("algo", 500);
 		cuenta.deposito("ahorro", 1000);
 		cuenta.retirar("corriente", 50);
 		cuenta.retirar("ayuda", 42);
-		cuenta.retirar("ahorro", 300);
+		cuenta1.deposito("algo", 500);
+		cuenta.deposito("ahorro", 1000);
+		cuenta1.retirar("corriente", 50);
+		cuenta.retirar("ayuda", 42);
+		cuenta1.retirar("ahorro", 300);
 		cuenta.retirar("ahorro", 1000000);
+		cuenta2.deposito("ahorro", 1000);
+		cuenta.retirar("corriente", 50);
+		cuenta2.retirar("ayuda", 42);
+		cuenta3.retirar("ahorro", 300);
 		cuenta.getBalances();
+		cuenta1.getBalances();
+		cuenta2.getBalances();
+		cuenta3.getBalances();
 	}
 }
